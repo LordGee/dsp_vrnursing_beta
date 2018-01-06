abc4eef27173fe0c62c39ca8d1535ff1ee600ca6
LordGee <gordon.johnson@mychaos.co.uk>
2018-01-06 17:25:19 +0000

    Play Audio Clips while Presenting Instructions
    + Starting instructions are now presented vocally and visually

--- DSP_NursingVR/Assets/Scripts/Task1.cs
+++ DSP_NursingVR/Assets/Scripts/Task1.cs
@@ -6,27 +6,55 @@ using UnityEngine.UI;
 public class Task1 : MonoBehaviour
 {
     public GameObject taskCanvas, spawnCanvas;
-    public string taskInstructions;
+    public AudioClip[] voiceOverAudioClips;
+    public string[] taskInstructions;
 
     private bool isActive;
-    private int itemCount;
+    private int itemCount, voiceIndex;
 
     void Start()
     {
         EventController.TriggerEvent(ConstantController.EV_SPAWN_COLLECTABLE);
-        spawnCanvas = GameObject.Find("StartInstructions");
 
-        taskCanvas.transform.Find("Instructions").GetComponent<Text>().text = taskInstructions;
+        spawnCanvas = GameObject.Find("StartInstructions");
         Instantiate(taskCanvas, spawnCanvas.transform);
-        StartCoroutine(DestroyCanvas());
+
+        voiceIndex = 0;
+        PlayAudioClips();
 
         isActive = true;
         itemCount = 0;
     }
 
+    private void PlayAudioClips()
+    {
+        if (voiceIndex != voiceOverAudioClips.Length)
+        {
+            GetComponent<AudioSource>().clip = voiceOverAudioClips[voiceIndex];
+            GetComponent<AudioSource>().Play();
+            GameObject.Find("Instructions").GetComponent<Text>().text = taskInstructions[voiceIndex];
+            StartCoroutine(PlayNextClip(GetComponent<AudioSource>().clip.length));
+            voiceIndex++;
+        }
+        else
+        {
+            StartCoroutine(DestroyCanvas());
+        }
+    }
+
+    private IEnumerator PlayNextClip(float _waitFor)
+    {
+        yield return  new WaitForSeconds(_waitFor);
+        if ( GetComponent<AudioSource>().isPlaying ) {
+            StartCoroutine(PlayNextClip(1f));
+        } else {
+            PlayAudioClips();
+        }
+    }
+
     private IEnumerator DestroyCanvas()
     {
-        yield return new WaitForSeconds(30f);
+        yield return new WaitForSeconds(2f);
         Destroy(spawnCanvas);
     }
 
