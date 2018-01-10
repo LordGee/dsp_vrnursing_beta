a9378f1c6b2e7152c917f4a9f641af276a59e8a7
LordGee <gordon.johnson@mychaos.co.uk>
2018-01-10 17:44:00 +0000

    Grab and Place objects
    + Added the ability to grab specific objects
    + when placed on target location object is repositioned to satisfy placement
    + Once repositioned the object is made to be no longer grabbable (is that a word!)
    + Added booling results array to the Task2 script
    + When object placed in correct destination  the results array is updated
    + Tests for win conditions is tested every frame, this should refacted to only on each object success, even though this is a small loop it will still be a performance saving no matter how small. TODO: do this refactor sooner rather then later.

---
+++ DSP_NursingVR/Assets/Scripts/Monitor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using VRTK;
+
+public class Monitor : MonoBehaviour
+{
+    void OnCollisionEnter(Collision _col)
+    {
+        if (_col != null && _col.transform.name == "SideUnit")
+        {
+            if ( !GetComponent<VRTK_InteractableObject>().IsGrabbed() )
+            {
+                transform.localPosition = new Vector3(0f, 0.015f, 0f);
+                transform.localRotation = new Quaternion(0f, 0f, 0f, 0f);
+                GetComponent<MeshCollider>().enabled = false;
+                GetComponent<Rigidbody>().useGravity = false;
+                GetComponent<Rigidbody>().isKinematic = true;
+                FindObjectOfType<Task2>().UpdateObjectResult(0);
+            }
+        }
+    }
+}
--- DSP_NursingVR/Assets/Scripts/Task2.cs
+++ DSP_NursingVR/Assets/Scripts/Task2.cs
@@ -2,5 +2,38 @@
 
 public class Task2 : MonoBehaviour
 {
+    private bool[] objectSuccess;
+    private bool winStatus;
 
+    void Start()
+    {
+        objectSuccess = new bool[2];
+        for (int i = 0; i < objectSuccess.Length; i++) {
+            objectSuccess[i] = false;
+        }
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < objectSuccess.Length; i++) {
+            if (!objectSuccess[i]) { winStatus = false; }
+        }
+
+        if (winStatus) { WinTask(); }
+        else
+        {
+            Debug.Log("Not won yet");
+        }
+    }
+
+    public void UpdateObjectResult(int _index)
+    {
+        objectSuccess[_index] = true;
+        Debug.Log("Bool is now " + objectSuccess[_index]);
+    }
+
+    private void WinTask()
+    {
+        Debug.Log("You Win!");
+    }
 }
